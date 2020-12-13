b2adfb0af559ca9363a15fa847f490c6a4b3d9b6
SECTION <akasection@outlook.com>
2020-12-13 16:55:40 +0700

    Fix style problem

--- components/Language.vue
+++ components/Language.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
-.language-selector.absolute.rounded-full.py-1.px-4.m-3.z-50.bg-white.border-2.border-rustic-200
+.language-selector.absolute.rounded-full.py-1.pl-4.pr-6.m-3.z-50.bg-white.border-2.border-rustic-200
   label.hidden.text-rustic-500(for="lang-selector" class="lg:inline") {{ $tc('choose' )}}
   select(id="lang-selector" v-model="lang" :placeholder="`Language: ${lang}`")
     option(class="lg:block" v-for="locale in availableLocales" :key="locale" :value="locale") {{ $tc(`${locale}_s`) }}
+  .triangle.w-4.relative.inline
 </template>
 
 <script lang="ts">
@@ -28,7 +29,36 @@ export default Vue.extend({
 </script>
 
 <style lang="stylus" scoped>
+select
+  // A reset of styles, including removing the default dropdown arrow
+  appearance none
+  // Additional resets for further consistency
+  background-color transparent
+  border none
+  padding 0 0.25rem 0 0
+  margin 0
+  // width 100%
+  font-family inherit
+  font-size inherit
+  cursor inherit
+  line-height inherit
+  grid-template-areas "select"
 
+  // &::-ms-expand
+  //   display none
+
+.triangle
+
+  &:after
+    content ""
+    width 0.5rem
+    height 0.5rem
+    display inline
+    clip-path polygon( 0 0%, 100% 50%, 0 100%)
+    position absolute
+    left 0
+    top 0.35rem
+    @apply bg-black
 </style>
 
 <i18n>
